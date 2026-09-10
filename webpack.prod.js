65f024a5fbb1b415a0972ee98040b167b351e708
Anthony De la cruz <277806914+anthony-dlc@users.noreply.github.com>
2026-09-10 13:29:57 -0400

    Initial commit

---
+++ webpack.prod.js
@@ -0,0 +1,21 @@
+import { merge } from "webpack-merge";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
+import common from "./webpack.common.js";
+
+export default merge(common, {
+  mode: "production",
+  devtool: "source-map",
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: ["...", new CssMinimizerPlugin()],
+  },
+});
